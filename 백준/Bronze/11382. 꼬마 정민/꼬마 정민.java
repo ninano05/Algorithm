5bb22fa3f62ca4a35df37bb25a0828d246ccd8b9
ninano05 <ohjy0530@gmail.com>
2025-12-06 16:22:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        bw.write((a+b+c)+"\n");
+        bw.flush();
+        
+        br.close();
+        bw.close();
+    }
+}
