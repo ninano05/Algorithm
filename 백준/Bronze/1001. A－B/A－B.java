300b2e5952659bb088ddcd125678f3f6f3ce3248
ninano05 <ohjy0530@gmail.com>
2025-12-06 14:16:21 +0900

    [Bronze V] Title: A-B, Time: 116 ms, Memory: 16100 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sub = Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken());
+        
+        bw.write(sub+"\n");
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
