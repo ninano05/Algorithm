c4fb023c23e27943dedf818bfe00718fe3146752
ninano05 <ohjy0530@gmail.com>
2025-12-06 14:07:58 +0900

    [Bronze V] Title: A+B, Time: 116 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sum = Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken());
+        
+        bw.write(sum+"\n");
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
