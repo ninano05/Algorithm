772b6b80ad5f3bbffb8f1c940b13b2e93c2e7156
ninano05 <ohjy0530@gmail.com>
2025-12-08 13:46:21 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("|\\_/|\n");
+        bw.write("|q p|   /}\n");
+        bw.write("( 0 )\"\"\"\\\n");
+        bw.write("|\"^\"`    |\n");
+        bw.write("||_/=\\\\__|\n");
+        
+        bw.flush();
+        bw.close();
+    }
+}
