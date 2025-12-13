97c09cfd655c26d075e90b07c49ebd7c8e5cef3e
ninano05 <ohjy0530@gmail.com>
2025-12-13 11:24:42 +0900

    [Bronze IV] Title: 영수증, Time: 112 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int total = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += (a*b);
+        }
+        bw.write(checkTotal(total, sum));
+        bw.flush();
+        
+        br.close();
+        bw.close();
+    }
+    
+    public static String checkTotal(int total, int sum) {
+        if(total == sum)
+            return "Yes";
+        else return "No";
+    }
+}
