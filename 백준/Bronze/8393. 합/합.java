dfacafb1288fe46f467c316452184cead32bcc23
ninano05 <ohjy0530@gmail.com>
2025-12-13 11:12:27 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 15796 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        
+        for(int i=1;i<n+1;i++) {
+            sum += i;
+        }
+        bw.write(sum+"\n");
+        bw.flush();
+        
+        bw.close();
+        br.close();
+    }
+}
