d4b562cef4c8f88ead51d35478da03c30d7845ea
ninano05 <ohjy0530@gmail.com>
2026-03-27 11:03:08 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -8,13 +8,13 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int row = 2; // 초기 상태
+        int row = 0; // 초기 상태
 
         // 한줄에 생기는 점의 개수를 계산한다.
         for(int i=0; i<N; i++) {
-            row = (row*2) -1;
+            row = (1 << N)+1;
         }
-        bw.write(row*row+"\n");
+        bw.write((int)Math.pow(row,2)+""); // Math.pow()는 double 타입으로 반환 된다. 
         bw.flush();
         bw.close();
         br.close();
