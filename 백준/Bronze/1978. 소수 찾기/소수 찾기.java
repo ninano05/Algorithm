746fb7764b7be3466dacbf4c7baea8f85060e36a
ninano05 <ohjy0530@gmail.com>
2026-03-27 16:00:33 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int count=0;
+
+        for(int i=0;i<N;i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(isPrime(num)) count++;
+        }
+        bw.write(count+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 1) return false;
+        for(int i=2; i*i<=n; i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
