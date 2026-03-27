63749ed5dc721ec47ac17366f805ca1cd9ca253a
ninano05 <ohjy0530@gmail.com>
2026-03-27 16:01:42 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -22,7 +22,7 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-        if(n == 1) return false;
+        if(n < 2) return false;
         for(int i=2; i*i<=n; i++) {
             if(n%i==0) return false;
         }
