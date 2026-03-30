682a78d4f77a7e95a3c0424e143ff695bf007cd1
ninano05 <ohjy0530@gmail.com>
2026-03-30 15:26:07 +0900

    [Bronze III] Title: 네 번째 점, Time: 120 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -7,30 +7,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] X = new int[3];
-        int[] Y = new int[3];
+        int x = 0;
+        int y = 0;
 
         StringTokenizer st;
         for(int i=0; i<3;i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            X[i] = Integer.parseInt(st.nextToken());
-            Y[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int x;
-        int y;
-
-        if(X[0] == X[1]) {
-            x = X[2];
-        } else {
-            if(X[0] == X[2]) x = X[1];
-            else x = X[0];
-        }
-        if(Y[0] == Y[1]) {
-            y = Y[2];
-        } else {
-            if(Y[0] == Y[2]) y = Y[1];
-            else y = Y[0];
+            x ^= Integer.parseInt(st.nextToken());
+            y ^= Integer.parseInt(st.nextToken());
         }
 
         bw.write(x+" "+y);
