8524a409724570d99b31a0e4a8a5414669946776
ninano05 <ohjy0530@gmail.com>
2026-04-01 12:46:30 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 184 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -19,12 +19,13 @@ public class Main {
         int x = 0;
         int y = 0;
 
+        loop:
         for(int i=-999; i<=999; i++) {
             for(int j=-999; j<=999; j++) {
                 if((a*i+b*j) == c && (d*i+e*j) == f) {
                     x = i;
                     y = j;
-                    break;
+                    break loop;
                 }
             }
         }
