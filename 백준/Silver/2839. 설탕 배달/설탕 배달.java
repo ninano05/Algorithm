6d767111d6dd26baf07b5c8001ef3037595d60ab
ninano05 <ohjy0530@gmail.com>
2026-04-01 15:39:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,34 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static char[][] board;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
+        int answer = -1;
 
-        int small= 0;
-        int big = 0;
-        int min = 0;
+        for(int five = n/5; five>=0; five--) {
+            int remain = n - (five*5);
 
-        loop:
-        for(int s=0; s<(n/3+1); s++) {
-            for(int b=0; b<(n/5+1); b++){
-                int weight = 3*s+5*b;
-                if(weight == n) {
-                    small = s;
-                    big = b;
-                    break loop;
-                }
+            if(remain % 3 == 0) {
+                int three = remain/3;
+                answer = five + three;
+                break;
             }
         }
-        if(small == 0 && big == 0) {
-            bw.write("-1");
-        } else {
-            bw.write(small + big+"");
-        }
+        bw.write(answer+"");
         bw.flush();
         bw.close();
         br.close();
