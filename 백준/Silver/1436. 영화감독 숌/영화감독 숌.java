5243157b737e9e9a353e84abff186bb9a1f1a551
ninano05 <ohjy0530@gmail.com>
2026-04-02 10:45:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 86264 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -8,18 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 영화 시리즈
-        int count = 1;
-        int title = 666;
+        int N = Integer.parseInt(br.readLine());
+        int series = 1; // 몇번 째 시리즈인지 카운트
+        int title = 666; // 1번 째 시리즈는 666이다.
 
-        while(count < n) {
-            title += 1;
-            String sTitle = String.valueOf(title);
-            if(sTitle.contains("666")) {
-                count++;
+        // 무식하게 돌려보기~
+        // 영화는 최소 666부터 시작한다 ~
+        while(series < N) {
+            title ++;
+            if(String.valueOf(title).contains("666")) {
+                series ++;
             }
         }
-
         bw.write(title+"");
         bw.flush();
         bw.close();
