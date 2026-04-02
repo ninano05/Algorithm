d2da4d5a7557b00b73adf0b4926a8fe58632b32a
ninano05 <ohjy0530@gmail.com>
2026-04-02 10:54:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,24 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static char[][] board;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int answer = -1;
+        int N = Integer.parseInt(br.readLine());
+        int num = -1;
 
-        for(int five = n/5; five>=0; five--) {
-            int remain = n - (five*5);
-
-            if(remain % 3 == 0) {
-                int three = remain/3;
-                answer = five + three;
-                break;
+        loop:
+        for(int s=0; s<=N/3; s++) { // small bag
+            for(int b=0; b<=N/5; b++){ // big bag
+                int weight = 3*s+5*b;
+                if(weight == N) {
+                    num = s+b; // 5Kg 짜리가 많을 때가, 가장 적은 가방 개수 => 바로 종료하기
+                    break loop;
+                }
             }
         }
-        bw.write(answer+"");
+
+        bw.write(num+"");
         bw.flush();
         bw.close();
         br.close();
