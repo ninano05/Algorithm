00b9c7ecedf1f7d398494a71b46ad644dcb487a4
ninano05 <ohjy0530@gmail.com>
2026-04-02 09:36:20 +0900

    [Bronze II] Title: 분해합, Time: 148 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -8,23 +8,22 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int min = 0; // 조건 만족 안하는 경우 초기값임(만족할 때만 값 업데이트)
-        int start = Math.max(0, N-String.valueOf(N).length()*9); // N의 자릿수가 999같이 가장 큰 경우에, 생성자가 작아지기 때문
+        int M = 0; // 생성자가 없는 경우 -> 있다면 생성자로 바꾸기
 
-        for(int i=start; i<=N; i++) {
+        for(int i=1; i<N; i++) {
             int sum = i;
             int temp = i;
-
-            while(temp != 0) {
-                sum += temp%10;
-                temp = temp/10;
+            while(temp > 0) {
+                sum = sum + temp % 10;
+                temp = temp / 10;
             }
-            if(sum == N) { // 조건을 만족하면 저장하고 빠져 나가기
-                min = i;
+            if(sum == N){
+                M = i; // M을 1부터 가정하고 커지기 때문에, 찾는 순간 종료하면 가장 작은 값이다.
                 break;
             }
         }
-        bw.write(min+"");
+
+        bw.write(M+"");
         bw.flush();
         bw.close();
         br.close();
