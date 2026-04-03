b4e8d18a8cff6cec9fd3c90e6fa63ac3eb043715
ninano05 <ohjy0530@gmail.com>
2026-04-03 11:08:44 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 26272 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -14,19 +14,17 @@ public class Main {
         for(int i=0; i<N; i++) { //듣도 못한 사람 입력
             set.add(br.readLine());
         }
-        int count = 0;
         ArrayList<String> list = new ArrayList<>();
         // 보도 못한 사람 입력 & 비교하여 중복 출력
         for(int i=0; i<M; i++) {
             String name = br.readLine();
             if(set.contains(name)) {
-                count++;
                 list.add(name);
             }
         }
         // 정렬
         Collections.sort(list);
-        sb.append(count).append("\n");
+        sb.append(list.size()).append("\n");
         for(String s : list) {
             sb.append(s).append("\n");
         }
