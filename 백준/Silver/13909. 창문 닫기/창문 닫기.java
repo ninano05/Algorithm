92bc7cd16b39ed054c098c8f16cd298d2d44f9ea
ninano05 <ohjy0530@gmail.com>
2026-04-06 17:00:06 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -9,15 +9,9 @@ public class Main {
 
         // 약수의 개수가 홀수인 창문만 열려있음
         // 약수의 개수가 홀수라는 뜻은 어떤 수의 제곱수라는 뜻이다.
-
         int N = Integer.parseInt(br.readLine());
-
-        int open = 1; // 1은 자기 자신 뿐이라 무조건 열려 있음 -> 포함해주기
-        // 제곱 수는 창문이 열려있다. 제곱수들만 구하면 된다.
-        for(int i=2; i*i<=N; i++) {
-            open++;
-        }
-        sb.append(open);
+        // N 이하의 제곱수는 N루트 개수만큼 나오기 때문
+        sb.append((int)Math.sqrt(N));
         System.out.print(sb);
         br.close();
     }
