8098bb7464a79b5f63fc7af75d8e41fe89abef39
ninano05 <ohjy0530@gmail.com>
2026-04-06 16:14:00 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 492 ms, Memory: 73624 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -13,19 +13,21 @@ public class Main {
 
             // N 보다 작은 소수 고르기
             boolean[] composite = new boolean[N+1];
+            composite[0] = true;
+            composite[1] = true;
 
             for(int i=2; i*i<=N; i++) {
-                for(int j=i*i; j<=N; j+=i) {
-                    if(!composite[j]) composite[j] = true; // 배수들은 소수 아님
+                if(!composite[i]) {
+                    for(int j=i*i; j<=N; j+=i) {
+                        if(!composite[j]) composite[j] = true; // 배수들은 소수 아님
+                    }
                 }
             }
             int count = 0;
             // 두 소수의 합이 N이라는 뜻은 n-소수 = 소수 이어야한다는 뜻이다.
             // 즉 절반에 대해서 (n-소수)가 소수인지 확인하면 된다.
             for(int i=2; i<=N/2; i++) {
-                if(!composite[i]) {
-                    if(!composite[N-i]) count++;
-                }
+                if(!composite[i] && !composite[N-i]) count++;
             }
             sb.append(count).append("\n");
         }
