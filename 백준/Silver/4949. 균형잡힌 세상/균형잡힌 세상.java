b8e0742d13559aa05de07d73e303136c1283fb34
ninano05 <ohjy0530@gmail.com>
2026-04-07 11:08:31 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 172 ms, Memory: 18940 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -10,38 +10,38 @@ public class Main {
         while(true) {
             String s = br.readLine();
             if(s.equals(".")) break;
-            Stack<Character> stack = new Stack<>();
-            boolean res = true;
-
-            for(int i=0; i<s.length(); i++) {
-                char c = s.charAt(i);
-                if(c == '(' ||  c == '[') {
-                    stack.push(c);
-                } else if(c == ')') {
-                    if(stack.isEmpty()) {
-                        res = false;
-                        break;
-                    }
-                    if(stack.pop() != '(') {
-                        res = false;
-                        break;
-                    }
-                } else if(c ==']') {
-                    if(stack.isEmpty()) {
-                        res = false;
-                        break;
-                    }
-                    if(stack.pop() != '[') {
-                        res = false;
-                        break;
-                    }
-                }
-            }
-            if(!stack.isEmpty()) res = false;
+            boolean res = getMatch(s);
+            
             if(res) sb.append("yes\n");
             else sb.append("no\n");
         }
         System.out.print(sb);
         br.close();
+    }
+    
+    public static boolean getMatch(String s) {
+        Stack<Character> stack = new Stack<>();
+        boolean res = true;
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' ||  c == '[') {
+                stack.push(c);
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.peek() != '(') {
+                    res = false;
+                    break;
+                }
+                stack.pop();
+            } else if(c ==']') {
+                if(stack.isEmpty() || stack.peek() != '[') {
+                    res = false;
+                    break;
+                }
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) res = false;
+        return res;
     }
 }
