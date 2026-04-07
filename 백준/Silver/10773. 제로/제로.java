24420e7d54503a7391efdf6e1e825da9c054f9be
ninano05 <ohjy0530@gmail.com>
2026-04-07 10:20:06 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23688 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,41 +1,26 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] s = new int[n];
-        
-        for(int i=0;i<n;i++) {
-            int num = Integer.parseInt(br.readLine());
-            s[i] = num;
-        }
-        
-        bw.write(solution(n, s)+"");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
-    public static int solution(int n, int[] s) {
+        StringBuilder sb = new StringBuilder();
+
+        int K = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        int answer = 0;
-        
-        for(int i=0;i<n;i++) {
-            if(s[i] == 0) {
-                stack.pop();
-            } else {
-                stack.push(s[i]);
-            }
+
+        for(int i=0; i<K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) stack.pop();
+            else stack.add(num);
         }
-        
-        for(int k : stack) {
-            answer += k;
+        long sum = 0;
+        for( int n : stack ) {
+            sum += n;
         }
-        
-        return answer;
+        sb.append(sum);
+        System.out.print(sb);
+        br.close();
     }
 }
