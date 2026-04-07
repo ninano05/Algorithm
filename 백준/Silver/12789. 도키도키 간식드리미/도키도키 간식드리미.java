e067e3ebf92e059bf7ce6eed11176cb3234bc6da
ninano05 <ohjy0530@gmail.com>
2026-04-07 12:23:26 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -2,54 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Queue<Integer> line;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        line = new ArrayDeque<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int snack = 1; // 현재 간식 받을 사람
+        Stack<Integer> stack = new Stack<>();
+        
         for(int i=0; i<N; i++) {
-            line.offer(Integer.parseInt(st.nextToken()));
+            int cur = Integer.parseInt(st.nextToken());
+            
+            if(cur == snack) {
+                snack++;
+            } else {
+                stack.push(cur);
+            }
+            
+            while(!stack.isEmpty() && stack.peek() == snack) { // 대기열 학생인 순서인 경우 모두 빼기
+                stack.pop();
+                snack++;
+            }
         }
-        if(getSnack(N)) sb.append("Nice");
+        if(stack.isEmpty()) sb.append("Nice");
         else sb.append("Sad");
 
         System.out.print(sb);
         br.close();
-    }
-
-    public static boolean getSnack(int N) {
-        int snack = 1; // 간식 받을 사람 번호
-        Stack<Integer> stack = new Stack<>();
-        boolean res = true;
-
-        while(true) {
-            // 대기열 점검
-            if(!stack.isEmpty() && stack.peek() == snack) {
-                stack.pop();
-                snack++;
-                continue;
-            }
-            // 줄 점검
-            if(!line.isEmpty() && line.peek() == snack) {
-                line.poll();
-                snack++;
-                continue;
-            } else {
-                if(!line.isEmpty() && !stack.isEmpty() && line.peek() > stack.peek()) {
-                    res = false;
-                    break;
-                }
-                if(!line.isEmpty()) stack.push(line.poll());
-            }
-            // 끝 도달
-            if(snack > N) break;
-        }
-
-        return res;
     }
 
 }
