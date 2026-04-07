222ab144761da99c0aa7f28287c6405ade9653a8
ninano05 <ohjy0530@gmail.com>
2026-04-07 17:49:20 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 168 ms, Memory: 14968 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -8,41 +8,31 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        Deque<int[]> que = new ArrayDeque<>();
+        Deque<int[]> deq = new ArrayDeque<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         // 풍선 번호 순서대로 큐에 담아주기
         for(int i=1; i<=N; i++) {
-            que.offer(new int[]{i, Integer.parseInt(st.nextToken())}); //i는 풍선 번호, 뒤는 종이번호
+            deq.offer(new int[]{i, Integer.parseInt(st.nextToken())}); //i는 풍선 번호, 뒤는 종이번호
         }
-        int target = 0; // 이동해야 하는 거리(=패스해야하는 풍선의 개수)
-        int dir = 0; // 방향 기록(0:오른쪽, 1: 왼쪽)
+
         // 풍선 터뜨리기
-        while(!que.isEmpty()) {
-            while(target != 0) {
-                if(target > 0) {
-                    que.offer(que.poll()); // 순서 넘어가기 양수 방향
-                    target--;
-                } else {
-                    que.offerFirst(que.pollLast()); // 순서 넘어가기 음수 방향
-                    target++;
+        while(!deq.isEmpty()) {
+            int[] cur = deq.pollFirst();
+            int index = cur[0]; // 풍선 번호
+            int move = cur[1]; // 이동해야 하는 거리
+
+            sb.append(index).append(" ");
+
+            if(deq.isEmpty()) break;
+            if(move > 0) {
+                for(int i=0; i<move-1; i++) {
+                    deq.offerLast(deq.pollFirst());
+                }
+            } else {
+                for(int i=0; i<-move; i++) { // 음수인 경우는 내가 뽑아야하는 것까지 맨 앞으로 땡겨온다.
+                    deq.offerFirst(deq.pollLast());
                 }
             }
-            int[] balloon;
-            if(dir == 0) {
-                balloon = que.poll();
-            } else {
-                balloon = que.pollLast();
-            }
-            // 다음 이동거리 (=지나가야 할 풍선의 개수)
-            if(balloon[1]> 0) { // 양수(오른쪽)
-                target = balloon[1]-1;
-                dir = 0;
-            }
-            else { // 음수(왼쪽)
-                target = balloon[1]+1;
-                dir = 1;
-            }
-            sb.append(balloon[0]).append(" ");
         }
         System.out.print(sb);
         br.close();
