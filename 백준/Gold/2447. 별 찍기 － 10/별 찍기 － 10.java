85fb6769dc8a8b87a8de9d128b4c3dae98bd8dc3
ninano05 <ohjy0530@gmail.com>
2026-04-13 14:24:02 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 272 ms, Memory: 47112 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     static char[][] star;
-    
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -14,15 +14,14 @@ public class Main {
         // 기본 바탕 채우기
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                star[i][j] = '*';
+                star[i][j] = ' ';
             }
         }
-        // 재귀로 공백 뚫기
-        getShape(0, 0, N);
+        // 재귀로 별 그리기
+        drawStar(0, 0, N);
+
         for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                sb.append(star[i][j]);
-            }
+            sb.append(star[i]);
             sb.append("\n");
         }
 
@@ -30,33 +29,21 @@ public class Main {
         br.close();
     }
 
-    // 가운데를 공백으로 바꾸는 로직
-    public static void getShape(int x, int y, int len) { // 현재 좌상단 x,y 좌표, 한변의 길이
-        if(len == 1) return;
-        // 공백이 생기는 좌상단 좌표
-        int startX = x + len/3;
-        int startY = y + len/3;
-
-        int nLen = len/3; // 다음 변의 길이이자, 현재 길이의 3등분
-
-        for(int i=startY; i<startY+nLen; i++) {
-            for(int j=startX; j<startX+nLen; j++) {
-                star[i][j] = ' ';
-            }
+    public static void drawStar(int x, int y, int len) {
+        if(len == 1) {
+            star[x][y] = '*';
+            return;
         }
 
-        // 8개 불러야 함
-        getShape(x, y, nLen);
-        getShape(x+nLen, y, nLen);
-        getShape(x+nLen*2, y, nLen);
+        int next = len/3;
 
-        getShape(x, y+nLen, nLen);
-        // 가운데 공백 공간은 제외
-        getShape(x+nLen*2, y+nLen, nLen);
-
-        getShape(x, y+nLen*2, nLen);
-        getShape(x+nLen, y+nLen*2, nLen);
-        getShape(x+nLen*2, y+nLen*2, nLen);
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                // 가운데 공백인 공간
+                if(i == 1 && j ==1) continue;
+                drawStar(x+i * next, y+j*next, next); // 여기서 (3,3) 인경우 제외하고 모두 반복문 8번 돌아감
+            }
+        }
     }
 
 }
