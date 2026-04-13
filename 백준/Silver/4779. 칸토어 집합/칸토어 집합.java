676d29067cbe450fcd73715875cb5a664dbece66
ninano05 <ohjy0530@gmail.com>
2026-04-13 12:09:11 +0900

    [Silver III] Title: 칸토어 집합, Time: 256 ms, Memory: 23552 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,34 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static char[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        
         String line;
         while((line = br.readLine()) != null) {
             int N = Integer.parseInt(line);
             int len = (int)Math.pow(3, N);
+            arr = new char[len];
+            // 초기값 채우기
+            for(int i=0; i<len; i++) {
+                arr[i] = '-';
+            }
+            // 가운데만 공백으로 바꾸기
+            repeat(0, len);
 
-            String res = repeat(len);
-            sb.append(res).append("\n");
+            sb.append(arr).append("\n");
         }
         System.out.print(sb);
         br.close();
     }
     // 재귀 돌리는 함수
-    public static String repeat(int len) { // 전체 길이 넣어주기
-        if (len == 1) return "-"; // N=0일때, 3의 0제곱 처리
-        else { // 3보다 큰 경우
-            // 다음 문자 처리 단위
-            int next = len/3;
-            // 공백 만들기
-            StringBuilder blank = new StringBuilder();
-            for(int i=0; i<next; i++) {
-                blank.append(" ");
-            }
-            return repeat(next)+blank+repeat(next);
+    public static void repeat(int start, int len) { // 전체 길이 넣어주기
+        if(len == 1) return;
+        int nLen = len/3; // 다음 길이이자, 현재 길이의 3등분
+
+        for(int i=start+nLen; i<start+2*nLen; i++) {
+            arr[i] = ' ';
         }
+
+        repeat(start, nLen);
+        repeat(start+2*nLen, nLen);
     }
 }
