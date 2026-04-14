90d039524f53fb797f7ceed28ce190d8ad457439
ninano05 <ohjy0530@gmail.com>
2026-04-14 17:22:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 17448 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dist; // 수빈이가 각 좌표로 이동하는 길의 최단 시간
+    static boolean[] visited; // 수빈이가 방문한 길 기록
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken()); // 도달하기 위한 목표
+
+        // 길 초기화
+        dist = new int[100001]; // 해당 좌표까지 가는데 걸리는 최단 시간
+        visited = new boolean[100001]; // 이전에 방문했던 곳 또 방문할 필요 없음(그렇게 되면 최단 시간이 아니기 때문)
+
+        // 수빈이가 방문하는 길 bfs 돌리기
+        bfs(N, target);
+        sb.append(dist[target]);
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    public static void bfs(int start, int target) { // 시작 지점
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visited[start] = true; // 방문 표시 완료
+        dist[start] = 0; //현재 위치라 이동 시간 x
+
+        while(!visited[target]) { //타겟을 방문했으면 종료
+            int cur = queue.poll(); //현재 위치 꺼내기
+
+            for(int i=0; i<3; i++) {
+                int next = nextMove(i, cur); // 현재 위치에서 이동할 수 있는 3가지 경우
+
+                if (next >= 0 && next <= 100000) { // 문제에서 주어진 범위
+                    if (!visited[next]) { // 방문 안했으면
+                        queue.offer(next); // 방문
+                        visited[next] = true; // 방문 처리
+                        dist[next] = dist[cur] + 1; // 최단 시간: 부모에서 이동 시간 +1
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static int nextMove(int method, int x) { // 이동 방식, 현재 좌표
+        if(method == 0) return x-1;
+        else if(method == 1) return x+1;
+        else return 2*x;
+    }
+
+}
