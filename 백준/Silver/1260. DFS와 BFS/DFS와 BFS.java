8ff7d19d355fb6ab55f563c56810601678c3688c
ninano05 <ohjy0530@gmail.com>
2026-04-14 14:46:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 204 ms, Memory: 20284 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,88 +2,79 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static boolean[] visitedDfs;
+    static boolean[] visitedBfs;
     static ArrayList<Integer>[] graph;
-    static boolean[] visitedDFS; //dfs 방문
-    static boolean[] visitedBFS; //bfs 방문
-    static ArrayList<Integer> recordDFS = new ArrayList<>(); //dfs 방문 기록
-    static ArrayList<Integer> recordBFS = new ArrayList<>(); //bfs 방문 기록
-    
+    static StringBuilder visitOrder;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        // 변수 입력 받기
-        int n = Integer.parseInt(st.nextToken()); //노드 개수
-        int m = Integer.parseInt(st.nextToken()); //간선 개수
-        int v = Integer.parseInt(st.nextToken()); //시작점
-        
-        //graph,visited 초기화
-        graph = new ArrayList[n+1];
-        visitedDFS = new boolean[n+1];
-        visitedBFS = new boolean[n+1];
-        for(int i=1;i<n+1;i++) {
+        int N = Integer.parseInt(st.nextToken()); // 노드 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선 개수
+        int start = Integer.parseInt(st.nextToken()); // 시작 좌표
+
+        // 변수 생성 및 초기화
+        visitedDfs = new boolean[N+1];
+        visitedBfs = new boolean[N+1];
+        graph = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        //간선 입력 받기
-        for(int i=0;i<m;i++) {
+
+        // 그래프 그리기 (간선 연결)
+        for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            // 그래프 양방향
             graph[a].add(b);
             graph[b].add(a);
         }
-        // 그래프 순서 정렬(작은 노드부터 방문하도록)
-        for(int i=1; i<n+1; i++) {
-            Collections.sort(graph[i]);
+        // 정점 번호가 작은 것부터 방문 -> 연결 노드 순서를 오름차순으로 정렬해두기
+        for(int i=1; i<=N; i++) {
+            graph[i].sort((a,b) -> a - b);
         }
-        
-        // dfs, bfs 실행
-        dfs(v);
-        bfs(v);
-        
-        // 결과 출력
-        for(int k : recordDFS) {
-            bw.write(k+" ");
-        }
-        bw.write("\n");
-        for(int j : recordBFS) {
-            bw.write(j+" ");
-        }
-        bw.flush();
-        bw.close();
+
+        // 방문 순서 기록할 sb
+        visitOrder = new StringBuilder();
+        // dfs , bfs 각각 돌리기
+        dfs(start);
+        visitOrder.append("\n");
+        bfs(start);
+
+        System.out.print(visitOrder);
         br.close();
     }
-    
+
     public static void dfs(int start) {
-        visitedDFS[start] = true;
-        recordDFS.add(start); //방문 기록
-        
-        for(int next : graph[start]) {
-            if(!visitedDFS[next]) {
-                dfs(next); //dfs로 돌아다니기
+        visitedDfs[start] = true; // 방문 처리
+        visitOrder.append(start).append(" "); // 방문 순서 기록하기
+
+        for(int next: graph[start]) {
+            if(!visitedDfs[next]) {
+                dfs(next);
             }
         }
     }
-    
+
     public static void bfs(int start) {
         Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(start);
-        visitedBFS[start] = true;
-        recordBFS.add(start); //방문 기록
-        
+        queue.offer(start); // 큐에 넣기
+        visitedBfs[start] = true; // 방문 처리
+        visitOrder.append(start).append(" "); // 방문 순서 기록
+
         while(!queue.isEmpty()) {
-            int current = queue.poll();
-            
-            for(int next : graph[current]) {
-                if(!visitedBFS[next]) {
-                    visitedBFS[next] = true;
-                    recordBFS.add(next);
-                    queue.offer(next);
+            int cur = queue.poll(); // 현재 이어서 탐색할 노드 빼기
+
+            for(int next: graph[cur]) {
+                if(!visitedBfs[next]) { // 방문 안 했으면
+                    queue.offer(next); // 큐에 넣기
+                    visitedBfs[next] = true; // 방문 처리
+                    visitOrder.append(next).append(" "); // 방문 순서 기록
                 }
             }
         }
     }
-    
 }
