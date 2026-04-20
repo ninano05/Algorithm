7cbe9f1fd1d441780e11b5d5d0f134cc8ff0a1f4
ninano05 <ohjy0530@gmail.com>
2026-04-20 17:23:00 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -7,58 +7,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        // +와 -를 사용해서 값을 최소로 만들기 위해서는
-        // - 뒤로 나오는 애들을 모두 더해 놓아야함
         // ex) 5-1+50-10+50-5+80
         // 이렇게 나오는 경우 - 빼고 연산을 모두 먼저 실행
         // ex) 5-(1+50)-(10+50)-(5+80)
 
-        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
 
-        StringBuilder part = new StringBuilder();
-        boolean minus = false; // -가 나왔는지 여부
         int res = 0;
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '-') {
-                if (minus) { // 이전에 - 부호가 나왔을 경우
-                    res -= plus(String.valueOf(part));
-                    part.setLength(0); // part 초기화
-                } else { // 이전에 - 부호가 안 나왔을 경우
-                    res += plus(String.valueOf(part));
-                    part.setLength(0); // part 초기화
-                    minus = true;
-                }
-            } else {
-                part.append(c);
-            }
+        // 처음 값은 더하기 연산
+        res += plus(st.nextToken());
+        // 빼기 연산 시작
+        while(st.hasMoreTokens()) {
+            res -= plus(st.nextToken());
         }
-        // 마지막꺼 빼주기 전체 +인 경우 처리
-        if(minus) { // -가 나왔던 경우
-            res -= plus(String.valueOf(part));
-        } else { // - 가 안 나왔던 경우
-            res += plus(String.valueOf(part));
-        }
-
         sb.append(res);
         System.out.print(sb);
         br.close();
     }
 
+    // 더하기만 하는 함수
     public static int plus(String s) {
-        StringBuilder curNum = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(s, "+");
         int res = 0;
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '+') {
-                res += Integer.parseInt(String.valueOf(curNum));
-                curNum.setLength(0);
-            } else {
-                curNum.append(c);
-            }
+        while(st.hasMoreTokens()) {
+            res += Integer.parseInt(st.nextToken());
         }
-        // 마지막 숫자 더하기
-        res += Integer.parseInt(String.valueOf(curNum));
         return res;
     }
 }
