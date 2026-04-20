6b3c3bb455de0af99d38aa3576067e5af1124086
ninano05 <ohjy0530@gmail.com>
2026-04-20 23:08:22 +0900

    [Silver III] Title: 주유소, Time: 384 ms, Memory: 35260 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -11,12 +11,12 @@ public class Main {
         // 지금보다 최소 금액이 나오면 주유소 바꾸기
 
         int N = Integer.parseInt(br.readLine());
-        int[] d = new int[N]; // cost[0]까지 가는데 거리가 0이기에, d[0]=0으로 잡는다.
+        int[] d = new int[N-1]; // cost[0]까지 가는데 거리가 0이기에, d[0]=0으로 잡는다.
         int[] cost = new int[N];
 
         // 거리와 가격 입력
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i=1; i<N; i++) { // i=0 일때 d[0]=0
+        for(int i=0; i<N-1; i++) { // i=0 일때 d[0]=0
             d[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine(), " ");
@@ -24,21 +24,16 @@ public class Main {
             cost[i] = Integer.parseInt(st.nextToken());
         }
 
-        long curDist = 0; // 지금까지 온 거리
         long res = 0; // 최종 비용
-        int min = cost[0]; // 현재 최소 가격 (초기값: 처음 주유소 가격)
+        int min = cost[0]; // 최소 가격 (초기가격: 처음 주유소 가격)
 
-        // 최소 가격이 나올때마다 앞으로 가는 거리 주유소 바꿔서 결제
-        for(int i=1; i<N; i++) {
-            curDist += d[i]; // 지금까지 온 거리
-            if(cost[i] < min) { // 이전에 최소 가격이 비해 기름이 싸짐(최소가 갱신)
-                res += min * curDist; // 지금까지 온 거리 기름값 결제
+        // 거리에 대해서 현재 최소 가격으로 계속 결제하면서 간다.
+        for(int i=0; i<N-1; i++) {
+            if(cost[i] < min) { // 최소 가격 갱신
                 min = cost[i];
-                curDist = 0; // 온 거리 초기화
             }
+            res += (long) min * d[i]; // 인덱스 차이가 있어, 최소 가격 갱신이 먼저이어야 함
         }
-        // 거리가 남아 있으면 남은 금액 계산 (주유소 비용 갱신 안 되었다는 뜻, 현재 최소 주유소 비용으로 계산)
-        res += curDist * min;
 
         sb.append(res);
         System.out.print(sb);
