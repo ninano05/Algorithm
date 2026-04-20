8ddd78dde6f37f2e9ed13d736fd8b10f495427f5
ninano05 <ohjy0530@gmail.com>
2026-04-20 17:38:38 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,37 +1,12 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        // ex) 5-1+50-10+50-5+80
-        // 이렇게 나오는 경우 - 빼고 연산을 모두 먼저 실행
-        // ex) 5-(1+50)-(10+50)-(5+80)
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-
-        int res = 0;
-        // 처음 값은 더하기 연산
-        res += plus(st.nextToken());
-        // 빼기 연산 시작
-        while(st.hasMoreTokens()) {
-            res -= plus(st.nextToken());
-        }
-        sb.append(res);
-        System.out.print(sb);
-        br.close();
-    }
-
-    // 더하기만 하는 함수
-    public static int plus(String s) {
-        StringTokenizer st = new StringTokenizer(s, "+");
-        int res = 0;
-        while(st.hasMoreTokens()) {
-            res += Integer.parseInt(st.nextToken());
-        }
-        return res;
-    }
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
