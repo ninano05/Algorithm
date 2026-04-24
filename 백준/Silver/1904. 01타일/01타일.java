a3bf402b16c964031d244551558f0f612dffd80c
ninano05 <ohjy0530@gmail.com>
2026-04-24 17:53:11 +0900

    [Silver III] Title: 01타일, Time: 144 ms, Memory: 41364 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        //dp[N]은 00과 1로 만들 수 있는 조합 수를 의미
+        Long[] dp = new Long[N+1]; // 인덱스 맞추기
+
+        // N번째 수를 만든다고 생각해보자
+        // 마지막이 1로 끝난다면 앞에서 N-1개로 만들 수 있는 숫자 조합의 수는 dp[N-1]이다.
+        // 마지막이 00으로 끝난다면 앞에서 N-2개로 만들 수 있는 숫자 조합의 수는 dp[N-2]이다.
+        // 따라서 dp[N] = dp[N-1] + dp[N-2]라는 결론이 도출된다.
+
+        for(int i=1; i<=N; i++) {
+            // 초기값 세팅
+            if(i == 1) dp[i] = 1L;
+            else if(i == 2) dp[i] = 2L;
+            else dp[i] = (dp[i-1] + dp[i-2])%15746;
+        }
+
+
+        sb.append(dp[N]);
+        System.out.print(sb);
+        br.close();
+    }
+}
