6fe7388e182764bacd2f11fb048e80fe4ebc9a9d
ninano05 <ohjy0530@gmail.com>
2026-04-28 13:52:34 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14936 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        // 포도주 잔의 개수
+        int n = Integer.parseInt(br.readLine());
+
+        // 포도 주 잔
+        int[] wine = new int[n+1];
+        // 포도주 채우기
+        for(int i=1; i<=n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+
+        // dp[i] i번째 포도주를 마신다고 했을 때 최대의 양
+        // i: 몇번째 포도주인지 idx
+        int[] dp = new int[n+1];
+
+        // 초기값 설정
+        dp[1] = wine[1];
+        // 최대로 마시는 포도주 양
+        int max = dp[1];
+        if(n>=2) { // n이 2보다 클 때만 초기값 설정
+            dp[2] = wine[1] + wine[2];
+            max = dp[2];
+        }
+        
+        // i번 포도주에서 고민 (마실까 안 마실까)
+        for(int i=3; i<=n; i++) {
+            // i번 포도주 마심, 이전 포도주 안 마심
+            int a = dp[i - 2] + wine[i];
+            // i번 포도주 마심, 이전 포도주 마심, 이전이전 포도주 안 마심
+            // 이전이전이전 포도주를 마셨다고 가정하는 이유는 최대한 많이 마셔야 최대이기 때문
+            int b = dp[i - 3] + wine[i - 1] + wine[i];
+            // i번 포도주 안 마심
+            int c = dp[i-1];
+            // 최대 결정
+            dp[i] = Math.max(Math.max(a,b),c);
+            // 최대 업데이트하기
+            max = Math.max(dp[i], max);
+        }
+        sb.append(max);
+        System.out.print(sb);
+        br.close();
+    }
+}
