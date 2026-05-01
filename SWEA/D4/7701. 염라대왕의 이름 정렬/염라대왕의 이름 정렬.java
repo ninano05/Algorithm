899866d87dabeee03fb57b9507444b4cfcdb2754
ninano05 <ohjy0530@gmail.com>
2026-05-01 11:00:24 +0900

    [D4] Title: 염라대왕의 이름 정렬, Time: 813 ms, Memory: 150,252 KB -BaekjoonHub

--- SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.java
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.java
@@ -14,26 +14,26 @@ class Solution
 			int N = Integer.parseInt(br.readLine()); // 명부 이름 개수
 			
 			// 이름 입력 받기
-			String[] name = new String[N];
+			Set<String> set = new HashSet<>();
 			for(int i=0; i<N; i++) {
-				name[i] = br.readLine();
+				set.add(br.readLine()); // 중복 제거
 			}
+			
+			// 중복 제거한 거 리스트로 옮기기
+			ArrayList<String> name = new ArrayList<>(set);
 			
 			// 길이로 정렬하기
 			// 길이가 같으면 사전 순으로 정렬
-			Arrays.sort(name, (a,b) -> {
+			Collections.sort(name, (a,b) -> {
 				if(a.length() == b.length()) return a.compareTo(b);
 				return a.length() - b.length();
 			});
 			
 			
+			// 출력
 			sb.append("#").append(t).append("\n");
-			sb.append(name[0]).append("\n");
-			// 중복 제거해서 출력
-			for(int i=1; i<N; i++) {
-				if(!name[i].equals(name[i-1])) {
-					sb.append(name[i]).append("\n");
-				}
+			for(String s : name) {
+				sb.append(s).append("\n");
 			}
 		}
 		System.out.print(sb);
