7db6b8fd2c43c6320f00a63e8bef02212cae85f9
ninano05 <ohjy0530@gmail.com>
2026-05-08 11:37:53 +0900

    [D3] Title: 진용이네 주차타워, Time: 129 ms, Memory: 33,408 KB -BaekjoonHub

--- SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.java
+++ SWEA/D3/9280. 진용이네 주차타워/진용이네 주차타워.java
@@ -11,7 +11,6 @@ class Solution
 	static int[] w;
 	static int[] car;
 	
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -25,13 +24,12 @@ class Solution
 			int m = Integer.parseInt(st.nextToken()); // 주차할 차량 수
 			
 			// 인덱스 1~n사용
-			park = new boolean[n+1]; // 인덱스 주차 여부
+			park = new boolean[n+1]; // 인덱스 주차 여부 (true: 사용중, false: 주차가능)
 			w = new int[n+1]; // 인덱스 요금
 			
 			// 주차장별 요금 입력 받기, true로 초기화
 			for(int i=1; i<=n; i++) {
 				w[i] = Integer.parseInt(br.readLine());
-				park[i] = true;
 			}
 			
 			// 차량 무게 인덱스1~m사용
@@ -43,7 +41,7 @@ class Solution
 			map = new HashMap<>(); // 차가 주차한 곳 정보 저장 (차,주차구역)
 			income = 0; // 벌어드릴 수익
 			waitQue = new ArrayDeque<>(); // 대기 차량 큐
-			int empty = n; // 주차 빈공간 개수
+			//int empty = n; // 주차 빈공간 개수
 			
 			// 차량 주차 시작 - 요금 계산
 			for(int i=0; i<m*2; i++) {
@@ -58,7 +56,7 @@ class Solution
 				}
 				// 주차 종료 후 차량 나감
 				else if (curCar <0) {
-					park[map.get(carNum)] = true; // 주차 공간 비우기
+					park[map.get(carNum)] = false; // 주차 공간 비우기
 					map.remove(carNum); // 차량 정보 삭제
 					
 					// 공간 생겼으니, 필요하면 주차시키기
@@ -82,7 +80,7 @@ class Solution
 		if(parkNum != -1) { // 주차 가능
 			int carN = waitQue.poll(); // 이번에 주차할 차량 꺼내기
 			
-			park[parkNum] = false; // 주차 공간 막기
+			park[parkNum] = true; // 사용 중 표시
 			map.put(carN, parkNum); // 주차 정보 저장
 			income += (w[parkNum]*car[carN]); // 요금 계산
 		}
@@ -92,7 +90,7 @@ class Solution
 	public static int canPark() {
 		// 전체 돌면서 가장 주차 가능한 작은 i 찾기
 		for(int i=1; i<=n; i++) {
-			if(park[i]) {
+			if(!park[i]) {
 				return i; // 공간이 있음 -> 주차 구역 반환
 			}
 		}
